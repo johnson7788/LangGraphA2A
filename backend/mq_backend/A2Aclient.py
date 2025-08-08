a24e6526a79c4ab6542731b5217f41e2f2c7aa24
Johnson <linghuchongxajh@qq.com>
2025-08-08 17:57:38 +0800

    2025-08-08_17:57

--- backend/mq_backend/A2Aclient.py
+++ backend/mq_backend/A2Aclient.py
@@ -93,7 +93,6 @@ class A2AClientWrapper:
                 params=MessageSendParams(**message_data)
             )
             stream_response = self.client.send_message_streaming(streaming_request)
-
             async for chunk in stream_response:
                 self.logger.info(f"输出的chunk内容: {chunk}")
                 chunk_data = chunk.model_dump(mode='json', exclude_none=True)
